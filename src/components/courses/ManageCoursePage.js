102ca7d82e150610f8e286680d7ca661dc460600
James Muir <jamesmuir@Jamess-MacBook-Pro.local>
2020-04-27 14:29:33 -0400

    testing

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -9,7 +9,7 @@ import Spinner from "../common/Spinner";
 import {toast} from "react-toastify";
 
 
-function ManageCoursePage({courses, authors, loadAuthors, loadCourses, saveCourse, history, ...props}){
+export function ManageCoursePage({courses, authors, loadAuthors, loadCourses, saveCourse, history, ...props}){
 
 
     const [course, setCourse] = useState({ ...props.course });
